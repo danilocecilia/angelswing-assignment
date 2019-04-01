c168583e4422ac3de4b8c310432d46ac1608c5c3
Danilo <danilo@Danilos-MacBook-Pro.local>
2019-04-01 11:43:32 -0700

    Modified answer to test1 to make it more clear to understand.

--- test1.js
+++ test1.js
@@ -13,7 +13,46 @@ if (x == "") {
 console.log(x);
 */
 
-// Outpu: 0
-// Answer: When you do equality using == they usually check for type only
-// And when you do === comparison you check not only for type but value also
+let x = 0;
 
+if (x == "") {
+  let x = 1;
+} else {
+  x = 3;
+}
+
+console.log(x);
+
+// Outpu: 0
+// Answer: the first if statement as the comparison is made using
+// two == signs, that means it tries to do the type conversion since 0 and "" is consider a falsy value, so translating it
+// the results for the comparison is (false==false).
+// as the let statement works on a block scope, doing let = 1 inside the if statement does not affect the value of the x.
+/*
+
+Falsy Values
+Okay, so why does false == 0 in JavaScript? It’s complex, but it’s because in JavaScript 0 is a falsy value.
+
+Type coercion will actually convert our zero into a false boolean, then false is equal to false.
+
+There are only six falsy values in JavaScript you should be aware of:
+
+false — boolean false
+0 — number zero
+“” — empty string
+null
+undefined
+NaN — Not A Number
+
+When comparing any of our first three falsy values with loose equality(==), they will always be equal! That’s because these values will all coerce 
+into a false boolean.
+
+false == 0
+// true
+0 == ""
+// true
+"" == false
+// true
+
+
+*/
